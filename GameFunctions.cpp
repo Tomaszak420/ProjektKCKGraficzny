5cebce01f91d73cf9e569f46ba2383cbae1694cb
larycho <sample@users.noreply.com>
2024-11-08 11:07:02 +0100

    Zmiana struktury GameFunctions.cpp
    Usunieto funkcje obslugiwane w pelni przez metody GameState.
    Dodano nowe funkcje i zmieniono istniejace w celu poprawienia
    czytelnosci kodu i/lub wprowadzenia wiekszego oddzielenia logiki
    programu od czesci graficznej.

--- GameFunctions.cpp
+++ GameFunctions.cpp
@@ -1,84 +1,60 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <ncurses.h>
 #include <fstream>
 #include <cstring>
+#include <vector>
+#include <algorithm>
+
 #include "MenuFunctions.h"
 #include "ScreenChoice.h"
+#include "Coordinates.h"
+#include "GameState.h"
 
 using namespace std;
 
-// Definicja struktury przechowującej współrzędne
-struct coordinates {
-    int x;
-    int y;
-};
+#define KOLUMNY 4
+#define RZEDY 4
+#define MAX_WYBRANYCH 16
 
-// Funkcja losująca litery na planszy
-void losujKosci(string litery[4][4], WINDOW* okno) {
-    const char* kostka[16][6] = {
-            {"R", "I", "F", "O", "B", "X"}, {"I", "F", "E", "H", "E", "Y"},
-            {"D", "E", "N", "O", "W", "S"}, {"U", "T", "O", "K", "N", "D"},
-            {"H", "M", "S", "R", "A", "O"}, {"L", "U", "P", "E", "T", "S"},
-            {"A", "C", "I", "T", "O", "A"}, {"Y", "L", "G", "K", "U", "E"},
-            {"Qu", "B", "M", "J", "O", "A"}, {"E", "H", "I", "S", "P", "N"},
-            {"V", "E", "T", "I", "G", "N"}, {"B", "A", "L", "I", "Y", "T"},
-            {"E", "Z", "A", "V", "N", "D"}, {"R", "A", "L", "E", "S", "C"},
-            {"U", "W", "I", "L", "R", "G"}, {"P", "A", "C", "E", "M", "D"}
-    };
+// Funkcja wyswietlajaca plansze
+void displayBoard(string board[4][4], WINDOW* okno) {
 
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            int randomIndex = rand() % 6;
-            litery[i][j] = kostka[i * 4 + j][randomIndex];
-            mvwprintw(okno, i + 1, j * 5 + 1, "| %s |", litery[i][j].c_str());
+    for (int i = 0; i < RZEDY; ++i) {
+        for (int j = 0; j < KOLUMNY; ++j) {
+            mvwprintw(okno, i + 1, j * 5 + 1, "| %s |", board[i][j].c_str());
         }
     }
     wrefresh(okno);
 }
 
-// Sprawdzenie, czy wybrane współrzędne były już zaznaczone
-bool hasBeenSelected(struct coordinates* coords_so_far, int selected_count, int cursor_y, int cursor_x) {
-    for (int i = 0; i < selected_count; ++i) {
-        if (coords_so_far[i].y == cursor_y && coords_so_far[i].x == cursor_x) {
-            return true;
-        }
-    }
-    return false;
+// Wyswietla liczbe znlezionych slow
+void updateFoundWordsCounterDisplay(int liczba_znalezionych_slow) {
+    mvprintw(0, COLS - 20, "Slowa znalezione: %d", liczba_znalezionych_slow);
+    refresh();
 }
 
-// Sprawdzenie, czy wybór jest sąsiadem ostatniej litery
-bool isValidSelection(struct coordinates* coords_so_far, int selected_count, int cursor_y, int cursor_x) {
-    if (selected_count == 0) {
-        return true; // Pierwszy wybór jest zawsze prawidłowy
+// Wyświetlanie listy znalezionych słów
+void wyswietlListeZnalezionychSlow(WINDOW *lista_okno, vector<string> found_words)
+{
+    werase(lista_okno);    // Czyszczenie okna
+    box(lista_okno, 0, 0); // Rysowanie ramki
+
+    mvwprintw(lista_okno, 0, 1, "Znalezione slowa");
+
+    for (int i = 0; i < found_words.size(); ++i)
+    {
+        mvwprintw(lista_okno, i + 1, 1, "%s", found_words[i].c_str());
     }
-
-    int last_x = coords_so_far[selected_count - 1].x;
-    int last_y = coords_so_far[selected_count - 1].y;
-
-    return abs(last_x - cursor_x) <= 1 && abs(last_y - cursor_y) <= 1;
+    wrefresh(lista_okno);
 }
 
-// Funkcja sprawdzająca, czy słowo znajduje się w słowniku
-bool sprawdzSlowoWSlowniku(const char* slowo) {
-    ifstream plik("../dictionary.txt");
-    if (!plik.is_open()) {
-        cerr << "Błąd otwierania pliku" << endl;
-        return false;
-    }
-
-    string linia;
-    while (getline(plik, linia)) {
-        if (linia == slowo) {
-            return true;
-        }
-    }
-    return false;
-}
-
-// Wyświetlanie interfejsu użytkownika
-void wyswietlUI(int liczba_znalezionych_slow) {
+// Wyswietla instrukcje gry
+void gameScreenInit() {
     mvprintw(0, 0, "Kontrolki: ");
     mvprintw(1, 0, "Strzalki - Poruszanie sie");
     mvprintw(2, 0, "Enter - zeby wybrac");
@@ -87,126 +63,156 @@ void wyswietlUI(int liczba_znalezionych_slow) {
     mvprintw(5, 0, "X - zeby zakonczyc");
 
     mvprintw(0, COLS / 2, "Boggle");
-    mvprintw(0, COLS - 20, "Slowa znalezione: %d", liczba_znalezionych_slow);
-    refresh();
+    mvprintw(0, COLS - 20, "Slowa znalezione: 0");
 }
 
-// Wyświetlanie listy znalezionych słów
-void wyswietlListeZnalezionychSlow(WINDOW* lista_okno, char znalezione_slowa[][20], int liczba_znalezionych) {
-    werase(lista_okno);  // Czyszczenie okna
-    box(lista_okno, 0, 0);  // Rysowanie ramki
+// Wyswietla plansze/aktualizuje jej wyglad
+void updateBoard(WINDOW *plansza_okno, GameState state, struct coordinates cursor)
+{
+    for (int i = 0; i < RZEDY; ++i)
+    {
+        for (int j = 0; j < KOLUMNY; ++j)
+        {
+            // Jesli kursor znajduje sie w tym miejscu
+            if (i + 1 == cursor.y && j * 5 + 1 == cursor.x)
+            {
+                wattron(plansza_okno, A_REVERSE | COLOR_PAIR(1)); // Podświetlenie kursora
+            }
+            // Jesli ta litera zostala wybrana
+            else if (state.hasBeenSelected( { i, j } ))
+            {
+                wattron(plansza_okno, A_REVERSE | COLOR_PAIR(2)); // Podświetlenie wybranej litery
+            }
 
-    mvwprintw(lista_okno, 0, 1, "Znalezione slowa");
-    for (int i = 0; i < liczba_znalezionych; ++i) {
-        mvwprintw(lista_okno, i + 1, 1, "%s", znalezione_slowa[i]);
+            mvwprintw(plansza_okno, i + 1, j * 5 + 1, "| %s |", state.board[i][j].c_str());
+            wattroff(plansza_okno, A_REVERSE | COLOR_PAIR(1));
+            wattroff(plansza_okno, A_REVERSE | COLOR_PAIR(2));
+        }
     }
-    wrefresh(lista_okno);
+    wrefresh(plansza_okno);
+}
+
+// Uaktualnia ekran przy restarcie gry
+void restartGameDisplay(GameState state, WINDOW *lista_okno, WINDOW *plansza_okno)
+{
+    displayBoard(state.board, plansza_okno);
+
+    wyswietlListeZnalezionychSlow(lista_okno, state.found_words);
+
+    move(LINES - 2, 0);
+    clrtoeol();
+}
+
+// Funkcja sprawdzająca, czy słowo znajduje się w słowniku
+bool sprawdzSlowoWSlowniku(string slowo) {
+    ifstream plik("./dictionary.txt");
+    if (!plik.is_open()) {
+        cerr << "Błąd otwierania pliku" << endl;
+        return false;
+    }
+    int length = slowo.length();
+    string linia;
+    int counter = 0;
+    while (getline(plik, linia)) {
+
+        // Wywolanie erase() sluzy do obciecia znaku '\r' ('\n' obcial juz getline)
+        linia.erase(remove(linia.begin(), linia.end(), '\r' ), linia.end());
+        if (linia == slowo) {
+            plik.close();
+            return true;
+        }
+    }
+
+    plik.close();
+    return false;
+}
+
+void tryAddSelectedWord(GameState *state, WINDOW *lista_okno)
+{
+    // Obsługa sprawdzania słowa
+    if (state->getSelectedLength() > 0)
+    {
+        string slowo = state->getSelectedWord();
+
+        if (sprawdzSlowoWSlowniku(slowo))
+        {
+            // Proba dodania slowa do listy
+            bool success = state->tryAddSelectedWord();
+
+            if (success)
+            {
+                mvprintw(LINES - 2, 0, "Slowo %s dodane do listy!", slowo.c_str());
+                clrtoeol();
+            }
+            else
+            {
+                mvprintw(LINES - 2, 0, "Slowo juz znalezione!");
+                clrtoeol();
+            }
+        }
+        else
+        {
+            mvprintw(LINES - 2, 0, "Slowo nie znalezione w slowniku!");
+            clrtoeol();
+        }
+
+        wyswietlListeZnalezionychSlow(lista_okno, state->found_words);
+
+        state->clearSelection();
+    }
 }
 
 // Funkcja główna z pętlą gry
-void gra(WINDOW* okno, WINDOW* lista_okno) {
-    const int MAX_WYBRANYCH = 16;
-    const int RZEDY = 4, KOLUMNY = 4;
+void enterInteractionLoop(WINDOW *plansza_okno, WINDOW *lista_okno, GameState state)
+{
+    struct coordinates cursor = {1, 1};
 
-    string litery[RZEDY][KOLUMNY];
-    const char* wybrane_litery[MAX_WYBRANYCH];
-    struct coordinates selected_coordinates[MAX_WYBRANYCH];
+    while (true)
+    {
 
-    char znalezione_slowa[20][20];  // Tablica na znalezione słowa
-    int liczba_wybranych = 0, liczba_znalezionych_slow = 0;
+        updateBoard(plansza_okno, state, cursor);
 
-    losujKosci(litery, okno);
-
-    int kursor_y = 1, kursor_x = 1;
-
-    while (true) {
-        // Wyświetlanie liter z podświetleniem
-        for (int i = 0; i < RZEDY; ++i) {
-            for (int j = 0; j < KOLUMNY; ++j) {
-                if (i + 1 == kursor_y && j * 5 + 1 == kursor_x) {
-                    wattron(okno, A_REVERSE | COLOR_PAIR(1));  // Podświetlenie kursora
-                } else if (hasBeenSelected(selected_coordinates, liczba_wybranych, i, j)) {
-                    wattron(okno, A_REVERSE | COLOR_PAIR(2));  // Podświetlenie wybranej litery
-                }
-                mvwprintw(okno, i + 1, j * 5 + 1, "| %s |", litery[i][j].c_str());
-                wattroff(okno, A_REVERSE | COLOR_PAIR(1));
-                wattroff(okno, A_REVERSE | COLOR_PAIR(2));
-            }
-        }
-        wrefresh(okno);
-
-        // Wyświetl UI
-        wyswietlUI(liczba_znalezionych_slow);
+        updateFoundWordsCounterDisplay(state.found_words.size());
 
         int ch = getch();
 
         // Obsługa ruchu kursora
-        if (ch == KEY_UP && kursor_y > 1) {
-            kursor_y--;
-        } else if (ch == KEY_DOWN && kursor_y < RZEDY) {
-            kursor_y++;
-        } else if (ch == KEY_LEFT && kursor_x > 1) {
-            kursor_x -= 5;
-        } else if (ch == KEY_RIGHT && kursor_x < 1 + (KOLUMNY - 1) * 5) {
-            kursor_x += 5;
-        } else if (ch == 10) {  // Enter
-            int letter_x = (kursor_x - 1) / 5;
-            int letter_y = kursor_y - 1;
-
-            if (isValidSelection(selected_coordinates, liczba_wybranych, letter_y, letter_x)) {
-                selected_coordinates[liczba_wybranych] = { letter_x, letter_y };
-                wybrane_litery[liczba_wybranych] = litery[letter_y][letter_x].c_str();
-                liczba_wybranych++;
-            }
-        } else if (ch == 'w' || ch == 'W') {
-            // Obsługa sprawdzania słowa
-            if (liczba_wybranych > 0) {
-                string slowo = "";
-                for (int i = 0; i < liczba_wybranych; ++i) {
-                    slowo += wybrane_litery[i];
-                }
-
-                if (sprawdzSlowoWSlowniku(slowo.c_str())) {
-                    bool found = false;
-                    for (int i = 0; i < liczba_znalezionych_slow; ++i) {
-                        if (strcmp(znalezione_slowa[i], slowo.c_str()) == 0) {
-                            found = true;
-                            break;
-                        }
-                    }
-
-                    if (!found) {
-                        strcpy(znalezione_slowa[liczba_znalezionych_slow], slowo.c_str());
-                        liczba_znalezionych_slow++;
-                        mvprintw(LINES-2, 0, "Slowo %s dodane do listy!",slowo.c_str());
-                        clrtoeol();
-                    } else {
-                        mvprintw(LINES-2, 0, "Slowo juz znalezione!");
-                        clrtoeol();
-                    }
-                } else {
-                    mvprintw(LINES-2, 0, "Slowo nie znalezione w slowniku!");
-                    clrtoeol();
-                }
-
-                wyswietlListeZnalezionychSlow(lista_okno, znalezione_slowa, liczba_znalezionych_slow);
-                liczba_wybranych = 0;
-                memset(selected_coordinates, 0, sizeof(selected_coordinates));
-            }
-        } else if (ch == 'r' || ch == 'R') {
-            // Restart gry
-            liczba_wybranych = 0;
-            liczba_znalezionych_slow = 0;
-            memset(selected_coordinates, 0, sizeof(selected_coordinates));
-            memset(znalezione_slowa, 0, sizeof(znalezione_slowa));
-            losujKosci(litery, okno);
-            wyswietlListeZnalezionychSlow(lista_okno, znalezione_slowa, liczba_znalezionych_slow);
-            move(LINES - 2, 0);
-            clrtoeol();
+        if (ch == KEY_UP && cursor.y > 1)
+        {
+            cursor.y--;
         }
-        else if (ch == 'x' || ch == 'X') {
+        else if (ch == KEY_DOWN && cursor.y < RZEDY)
+        {
+            cursor.y++;
+        }
+        else if (ch == KEY_LEFT && cursor.x > 1)
+        {
+            cursor.x -= 5;
+        }
+        else if (ch == KEY_RIGHT && cursor.x < 1 + (KOLUMNY - 1) * 5)
+        {
+            cursor.x += 5;
+        }
+        else if (ch == 10) // Enter
+        {
+            state.tryAddLetter(cursor);
+        }
+        else if (ch == 'w' || ch == 'W')
+        {
+            // TODO
+            tryAddSelectedWord(&state, lista_okno);
+        }
+        else if (ch == 'r' || ch == 'R')
+        {
+            state.restartGame();
+            restartGameDisplay(state, lista_okno, plansza_okno);
+        }
+        else if (ch == 'x' || ch == 'X')
+        {
             break;
         }
+
+        int a = 5;
     }
 }
 
@@ -214,23 +220,29 @@ ScreenChoice game() {
 
     clear();
     refresh();
-    
-    // Utworzenie okien do gry
-    int wysokosc, szerokosc;
-    getmaxyx(stdscr, wysokosc, szerokosc);
 
-    int start_y = (wysokosc - 4) / 2;
-    int start_x = (szerokosc - 8) / 2;
-    WINDOW* okno = newwin(6, 22, start_y - 1, start_x - 1);
-    refresh();
-    box(okno, 0, 0);
-    wrefresh(okno);
+    GameState state;
 
-    WINDOW* lista_okno = newwin(20, 20, 1, szerokosc - 20);
+    int start_y = (LINES - 4) / 2;
+    int start_x = (COLS - 8) / 2;
+
+    WINDOW* plansza_okno = newwin(6, 22, start_y - 1, start_x - 1);
+    //refresh();
+    box(plansza_okno, 0, 0);
+    wrefresh(plansza_okno);
+
+    WINDOW* lista_okno = newwin(20, 20, 1, COLS - 20);
     box(lista_okno, 0, 0);
     mvwprintw(lista_okno, 0, 1, "Znalezione slowa");
     wrefresh(lista_okno);
 
-    gra(okno, lista_okno);
+    state.fillBoard();
+
+    displayBoard(state.board, plansza_okno);
+
+    gameScreenInit();
+
+    enterInteractionLoop(plansza_okno, lista_okno, state);
+
     return EXIT;
 }
