0ff14d295fea935d83b23a7af3553ed354f69821
Tomasz Mlyn <tomaszm2021@gmail.com>
2024-11-20 21:14:18 +0100

    Test Google (średnio działające)

---
+++ testygoogle/cmake-build-debug/googletest-1.15.2/googletest-1.15.2/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/tomaszak/Pulpit/ProjektKCK/testygoogle/googletest-1.15.2/googletest-1.15.2/googlemock
+# Build directory: /home/tomaszak/Pulpit/ProjektKCK/testygoogle/cmake-build-debug/googletest-1.15.2/googletest-1.15.2/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ testygoogle/cmake-build-debug/googletest-1.15.2/googletest-1.15.2/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tomaszak/Pulpit/ProjektKCK/testygoogle/googletest-1.15.2/googletest-1.15.2/googletest
+# Build directory: /home/tomaszak/Pulpit/ProjektKCK/testygoogle/cmake-build-debug/googletest-1.15.2/googletest-1.15.2/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
